fe743769fba825f445b9d41187f18426770236b6
tmntee <107428229+tmntee@users.noreply.github.com>
2024-04-03 21:16:14 -0400

    Create 16x16 grid with JS

---
+++ script.js
@@ -0,0 +1,20 @@
+const container = document.querySelector("div#container");
+
+const containerRows = [];
+
+for (let j = 0; j < 16; j++)
+{   
+    const tempRow = document.createElement("div");
+    tempRow.id = "grid-row";
+    containerRows.push(tempRow);
+
+    container.appendChild(tempRow);
+
+    for (let i = 0; i < 16; i++)
+    {
+        let tempDiv = document.createElement('div');
+        tempDiv.id = 'grid-div';
+
+        containerRows[j].appendChild(tempDiv);
+    }
+}
