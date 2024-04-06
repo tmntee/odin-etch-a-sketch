cc4477500fd358a08633b2f7f7f40ffd89ee5654
tmntee <107428229+tmntee@users.noreply.github.com>
2024-04-06 00:07:51 -0400

    Add button that lets user change canvas size. Create functions for grid creation.

--- script.js
+++ script.js
@@ -1,21 +1,63 @@
 const container = document.querySelector("div#container");
 
-const containerRows = [];
+let gridWidth = 512;
+let gridSize = 16;
 
-for (let j = 0; j < 16; j++)
+
+function generateGrid(size)
 {   
-    const tempRow = document.createElement("div");
-    tempRow.id = "grid-row";
-    containerRows.push(tempRow);
+    const containerRows = [];
+    
+    for (let j = 0; j < size; j++)
+    {   
+        const tempRow = document.createElement("div");
+        tempRow.id = "grid-row";
+        containerRows.push(tempRow);
 
-    container.appendChild(tempRow);
+        container.appendChild(tempRow);
 
-    for (let i = 0; i < 16; i++)
-    {
-        let tempDiv = document.createElement('div');
-        tempDiv.id = 'grid-div';
-        tempDiv.addEventListener('mouseover', func = () => {tempDiv.style.backgroundColor = "purple"});
+        for (let i = 0; i < size; i++)
+        {
+            let tempDiv = document.createElement('div');
+            tempDiv.id = 'grid-div';
+            let divSize = gridWidth / size;
+            let divSizeString = divSize + "px";
+            tempDiv.style.width = divSizeString;
+            tempDiv.style.height = divSizeString;
+            tempDiv.addEventListener('mouseover', func = () => {tempDiv.style.backgroundColor = "purple"});
 
-        containerRows[j].appendChild(tempDiv);
+            containerRows[j].appendChild(tempDiv);
+        }
     }
 }
+
+function clearGrid()
+{
+    while (container.firstChild)
+    {
+        container.removeChild(container.firstChild);
+    }
+}
+
+generateGrid(gridSize);
+
+
+const cSizeButton = document.querySelector("button#c-size");
+
+cSizeButton.addEventListener('click', func = () => { 
+    let num = 0;
+    do {
+        num = prompt("Enter pixel dimension: ")
+    } while (num > 100);
+    
+    gridSize = num;
+    clearGrid();
+    generateGrid(gridSize);
+})
+
+// last updated 4/3/2024 
+// coded button and working on onclick function for button,
+// need to delete grid and reload a grid with inputted number
+// IDEA: make a generate() function from grid generation at the beginning
+// that takes number parameter and call generate() in onClick function
+
